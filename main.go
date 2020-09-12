0d99338bf4bf494605a74baf8f9ffc3227fc8a68
apple <apple@juicyfruit.local>
2020-09-12 02:05:23 -0500

    added code

---
+++ main.go
@@ -0,0 +1,19 @@
+import (
+	"crypto/tls"
+	"fmt"
+	"net/http"
+)
+var tlsCert = /path/to/cert
+var tlsKey = /path/to/key
+
+func main() {
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      nil,
+	}
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("*"))))
+	err := srv.ListenAndServeTLS("tlsCert", "tlsKey")
+	if err != nil {
+		fmt.Printf("could not serve tls")
+	}
+}
