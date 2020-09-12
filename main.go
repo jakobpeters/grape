f9615fc2023c6b6142d95bac583fe5133c8ad24f
apple <apple@juicyfruit.local>
2020-09-12 02:09:21 -0500

    corrections to main

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"crypto/tls"
 	"fmt"
 	"net/http"
 )
-var tlsCert = /path/to/cert
-var tlsKey = /path/to/key
+
+var tlsCert = "/path/to/cert"
+var tlsKey = "/path/to/key"
 
 func main() {
 	srv := &http.Server{
-		Addr:         ":443",
-		Handler:      nil,
+		Addr:    ":443",
+		Handler: nil,
 	}
 	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("*"))))
 	err := srv.ListenAndServeTLS("tlsCert", "tlsKey")
